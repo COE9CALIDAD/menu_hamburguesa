b1d1fbe276ec074112e2a079b2efd0411792145c
Andres Nicolas Salcedo Torres <ingauxiliar.coe9@MHCINGENIEROS.local>
2024-07-11 16:30:19 -0500

    Agregue la animacion del botoncito

--- dom/menu-ham.js
+++ dom/menu-ham.js
@@ -5,6 +5,7 @@ export default function hamburgerMenu(panelBtn, panel) {
     d.addEventListener("click", (e) => {
         if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
             d.querySelector(panel).classList.toggle("is-active");
+            d.querySelector(panelBtn).classList.toggle("is-active");
         }
     });
 }
