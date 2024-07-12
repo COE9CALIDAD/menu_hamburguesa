81d09ef13e62365ed68b7e878052c085622d606a
Andres Nicolas Salcedo Torres <ingauxiliar.coe9@MHCINGENIEROS.local>
2024-07-12 11:10:12 -0500

    Se puede quitar el panel una vez activado

--- dom/index_dom.js
+++ dom/index_dom.js
@@ -3,5 +3,5 @@ import hamburgerMenu from "./menu-ham.js";
 const d = document;
 /*Aquí se complicó la cosa, no entiendo, tiene que ver con SELECTORES*/
 d.addEventListener("DOMContentLoaded", (e)=>{
-    hamburgerMenu(".panel-btn",".panel");
+    hamburgerMenu(".panel-btn",".panel", ".menu a");
 })
--- dom/menu-ham.js
+++ dom/menu-ham.js
@@ -1,13 +1,18 @@
 /*Este archivo es para crear la función que vamos a usar en el index-dom*/
-export default function hamburgerMenu(panelBtn, panel) {
+export default function hamburgerMenu(panelBtn, panel, menuLink) {
     const d = document; /*Esto es para simplificar todo el dominio del dom, que siempre inicia con document.*/
 
     d.addEventListener("click", (e) => {
         if(e.target.matches(panelBtn) || e.target.matches(`${panelBtn} *`)){
             d.querySelector(panel).classList.toggle("is-active");
-            d.querySelector(panelBtn).classList.toggle("is-active");
+            d.querySelector(panelBtn).classList.toggle("is-active"); /*Esto es para que también se "anime" la botón, al igual que para apareerlo solo se pone is-active*/
+        }
+        /*Esto es para que se retire el menú cuando hagamos clic en otro lado o en els vinculos*/
+        if(e.target.matches(menuLink)){
+            d.querySelector(panel).classList.remove("is-active");
+            d.querySelector(panelBtn).classList.remove("is-active"); /*Esto es para que también se "anime" la botón, al igual que para apareerlo solo se pone is-active*/
         }
     });
 }
 
-/*Si al hacer clic la*/
+/*Si se hace clic al botón de nuevo se restauran las clases? Que pasó ahí?*/
